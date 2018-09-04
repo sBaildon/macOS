3485c708b7f0740b3a5021e37911df2a288c3bd7
Sean Baildon <sean@baildon.co>
2018-09-04 23:26:36 +0100

    init

---
+++ install.sh
@@ -0,0 +1,6 @@
+echo "Installing hoebrew..."
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo "Installing ansible..."
+brew install ansible
+
