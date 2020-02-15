ac803e226b98bfd736168bf8f1d0c19a8381c58a
Sean Baildon <sean@baildon.co>
2020-02-15 21:30:55 +0000

    better install

--- install.sh
+++
@@ -1,6 +0,0 @@
-echo "Installing hoebrew..."
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-echo "Installing ansible..."
-brew install ansible
-
